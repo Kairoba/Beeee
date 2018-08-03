fab6e39345c2f0fc234dc0a2a1f49ea55af97ec3
Kairoba <kairoba0815@gmail.com>
2018-08-03 16:42:22 +0900

    Update BeeBot-Broadcast.js
    Refactoring 1

--- BeeBot-Broadcast.js
+++ BeeBot-Broadcast.js
@@ -31,14 +31,8 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB) {
   msg = msg.trim();
   var id = msg.split(' ');
 
-  if (msg == "/켜기" && sender == "소가/트위치"|sender == "닭강정/트위치"|sender == "마또리/트위치 준비중") {
-    botOFF = false;
-} else if (msg == "/끄기" && sender == "소가/트위치"|sender == "닭강정/트위치"|sender == "마또리/트위치 준비중") {
-    botOFF = true;
-}
-if (botOFF) return;
-
 var senderInfo = sender.split("/")
+var sender0 = senderInfo[0];
 var senderNick = id[1] == senderInfo[0].trim() ? senderInfo[0].trim() : null;
 var senderId = id[2];
 
@@ -47,7 +41,16 @@ var twidtxt = File.read("/storage/emulated/0/BeeBot/twid.txt");
 var twnicksplit = twnicktxt.split(" ");
 var twidsplit = twidtxt.split(" ");
 
-if(id[0] == "/읽기" && sender == "마또리/트위치 준비중"){
+if(sender0 == "소가"|sender0 == "닭강정"|sender0 == "마또리"){
+if (msg == "/켜기") {
+    botOFF = false;
+} else if (msg == "/끄기") {
+    botOFF = true;
+}
+}
+if (botOFF) return;
+
+if(id[0] == "/읽기" && sender0 == "마또리"){
     while(twnick.length > 0) {
     twnick.pop();
     twid.pop();
@@ -59,16 +62,24 @@ if(id[0] == "/읽기" && sender == "마또리/트위치 준비중"){
     replier.reply("대장님! 정상적으로 읽어왔습니다!")
 }
 
+if(room == "트위치 스트리머 서로 도와요!"|room == "스트리머 자유 채팅방"){
 if(id[0] == "/등록"&& id.length >= 1){
 if (senderNick != null && senderId != null) {
-     twnick.push(senderNick);
-     twid.push(senderId);
+     twnick.push(senderNick); twid.push(senderId);
      replier.reply(senderNick+"님 꿀단지에 등록 완료!")
 }else if(senderNick != null && isEmpty(senderId) ){
 replier.reply("신속하게! 아이디를 적으라구!\n양식: /등록+톡방 닉+트위치 ID\n등록 후, /방송+할 말");
 }else if(isEmpty(senderNick) && isEmpty(senderId)){
 replier.reply("톡방 닉네임을 적어주세요!\n양식: /등록+톡방 닉+트위치 ID\n등록 후, /방송+할 말");
-}
+}}
+if(id[0] == "/방종"){
+  if(twliveNick.indexOf(senderInfo[0].trim()) != -1){
+    twliveid.splice(twliveNick.indexOf(senderInfo[0].trim()),1);
+    twliveNick.splice(twliveNick.indexOf(senderInfo[0].trim()),1);
+    replier.reply("방종 처리되었습니다!\n오늘도 수고하셨어요!");
+}else{
+    replier.reply("방종 처리가 되지 않았어요!\n'마또리'에게 문의해주세요. :p");
+}}
 }
 
 if(id[0] == "/방송" && room == "트위치 스트리머 서로 도와요!"){
@@ -91,33 +102,19 @@ replier.reply("twitch.tv/" + twid[twnick.indexOf(senderInfo[0].trim())] + blank 
 replier.reply("등록이 필요합니다.\n정찰대의 규율을 깔보지 마시길!\n양식: /등록+톡방 닉+트위치 ID\n등록 후, /방송+할 말");
 }
 }
-if(id[0] == "/방송" && isEmpty(id[1]) && room == "앙 테스트"){
-if(twnick.indexOf(senderInfo[0].trim()) != -1){
-      twliveNick.push(senderInfo[0].trim())
-      twliveid.push(twid[twnick.indexOf(senderInfo[0].trim())])
-      replier.reply("대장님! 등록 완료!")
-}}
-if(id[0] == "/방송" && room == "앙 테스트"){
+if(room == "앙 테스트"){
+if(id[0] == "/방송"){
 if(twnick.indexOf(id[1]) != -1){
       twliveNick.push(id[1])
       twliveid.push(twid[twnick.indexOf(id[1])])
       replier.reply("대장님! "+id[1]+" 님 등록 완료!")
-}
-}
-if(id[0] == "/방종" && room == "트위치 스트리머 서로 도와요!"|room == "스트리머 자유 채팅방"){
-  if(twliveNick.indexOf(senderInfo[0].trim()) != -1){
-    twliveid.splice(twliveNick.indexOf(senderInfo[0].trim()),1);
-    twliveNick.splice(twliveNick.indexOf(senderInfo[0].trim()),1);
-    replier.reply("방종 처리되었습니다!\n오늘도 수고하셨어요!");
-}else{
-    replier.reply("방종 처리가 되지 않았어요!\n'마또리'에게 문의해주세요. :p");
 }}
-if(id[0] == "/방종" && sender == "마또리/트위치 준비중" && room == "앙 테스트"){
+if(id[0] == "/방종"){
 if(twliveNick.indexOf(id[1]) != -1){
       twliveid.splice(twliveNick.indexOf(id[1],1));
       twliveNick.splice(twliveNick.indexOf(id[1],1));
       replier.reply("대장님! "+id[1]+" 님 방종 완료!")
-}
+}}
 }
 
 if(id[0] == "/생방송" && room == "스트리머 자유 채팅방"|room == "앙 테스트"){
@@ -131,13 +128,13 @@ if(id[0] == "/생방송" && room == "스트리머 자유 채팅방"|room == "앙
     }else if(id[0] == "/생방송" && room == "트위치 스트리머 서로 도와요!"){
     replier.reply("자유 방에서만 가능하다구!")
 }
-if(id[0] == "/닉네임" && sender == "마또리/트위치 준비중"){
- replier.reply("[Bee Bot] 등록 닉네임 목록","/n" + twnick.join(", "));
+if(sender0 == "마또리"){
+if(id[0] == "/닉네임"){
+ replier.reply("[Bee Bot] 닉네임 등록 DB","\n" + twnick.join(", "));
  Utils.compress();
 }
-
-if(id[0] == "/아이디" && sender == "마또리/트위치 준비중"){
- replier.reply("[Bee Bot] 등록 아이디 목록","/n" + twid.join(", "));
+if(id[0] == "/아이디"){
+ replier.reply("[Bee Bot] 아이디 등록 DB","n" + twid.join(", "));
 Utils.compress();
-}
+}}
 }
